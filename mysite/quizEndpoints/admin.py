4c8328b731bb1e218d6c457b9d8aec0e3b52e026
soulting <andrzej_insadowski@onet.pl>
2024-01-04 14:46:58 +0100

    Added models for the database for django, and added the django structure

--- mysite/quizEndpoints/admin.py
+++ mysite/quizEndpoints/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Quiz, Users, Question
 # Register your models here.
+
+admin.site.register(Quiz)
+admin.site.register(Users)
+admin.site.register(Question)
